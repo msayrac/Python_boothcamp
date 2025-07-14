95a354e092ccbd09ac28cc4d403096aa942820a6
msayrac <msayracode@gmail.com>
2025-07-14 11:55:43 +0300

    python_boothcamp commit 1

---
+++ import_python.py
@@ -0,0 +1,10 @@
+
+homework_assignment_grades = {
+    "homework_1":85,
+    "homework_2":100,
+    "homework_3":81
+}
+
+import grade_average_service
+
+grade_average_service.calculate_homework(homework_assignment_grades)
